c808a05f0c8aa5b73ce305c017f927435207e6d1
Kanishk Sachdeva <kskanu2020@gmail.com>
2021-10-03 20:25:42 -0400

    error app.js resolved

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
-const port = 3000;
+// const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,9 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
